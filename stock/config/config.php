e59e799cee74dd2ea9b400dd2c015e198cf5ab01
susmita7 <58128007+susmita7@users.noreply.github.com>
2020-07-07 22:20:51 +0530

    project final

--- stock/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-      define("HOSTNAME", "localhost") ; /*---------------to define constant use define method in php --------*/
-      define("USERNAME", "root") ;
-      define("PASSWORD", "") ;
-      define("DBNAME", "inventory") ;
-
-
-      $con = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DBNAME);
--- stock/js/login.js
+++
@@ -1,3 +0,0 @@
-let tl_1=gsap.timeline({defaults:{duration:1.1}});
-
-tl_1.from('#text_left' , {opacity:0 , y:-100 , stagger:.4})
